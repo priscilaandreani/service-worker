8ec9e676716b5defd28a9840b6be5c9da9fd358b
Priscila Andreani <priscila.andreani@grupoboticario.com.br>
2024-06-25 15:53:31 -0300

    feat: cache requests in service worker

--- service-workers-offline/web/js/sw.js
+++ service-workers-offline/web/js/sw.js
@@ -1,8 +1,28 @@
 "use strict";
 
-const version = 2;
+const version = 5;
 var isOnline = true;
 var isLoggedIn = false;
+var cacheName = `ramblings-${version}`;
+
+var urlsToCache = {
+  // urls as clients will request them
+  loggedOut: [
+    "/",
+    "/about",
+    "/contact",
+    "/404",
+    "/login",
+    "/offline",
+    "/js/blog.js",
+    "/js/home.js",
+    "/js/login.js",
+    "/js/add-post.js",
+    "/css/style.css",
+    "/images/logo.gif",
+    "/images/offline.png",
+  ],
+};
 
 self.addEventListener("install", onInstall);
 self.addEventListener("activate", onActivate);
@@ -12,6 +32,8 @@ main().catch(console.error);
 
 async function main() {
   console.log(`Service Worker (${version}) is starting...`);
+  await clearCaches();
+  await cacheLoggedOutFiles();
   await sendMessage({ requestStatusUpdate: true });
 }
 
@@ -47,5 +69,52 @@ function onMessage({ data }) {
 
 async function handleActivation() {
   await clients.claim(); // take control of the all the pages immediately
+  await cacheLoggedOutFiles(/* forcedRelod=*/ true);
   console.log(`Service Worker (${version}) is activated.`);
+}
+
+async function cacheLoggedOutFiles(forceReload = false) {
+  var cache = await caches.open(cacheName);
+
+  return Promise.all(
+    urlsToCache.loggedOut.map(async function requestFile(url) {
+      try {
+        let res;
+        if (!forceReload) {
+          res = await cache.match(url);
+          if (res) {
+            return res;
+          }
+        }
+
+        let fetchOptions = {
+          method: "GET",
+          cache: "no-cache", // dont use the browser cache
+          credentials: "omit",
+        };
+
+        res = await fetch(url, fetchOptions);
+        if (res.ok) {
+          await cache.put(url, res); // you almost always wanna do a clone on that response
+        }
+      } catch {}
+    })
+  );
+}
+
+async function clearCaches() {
+  var cacheNames = await caches.keys();
+  var oldCacheNames = cacheNames.filter(function matchOldCache(cacheName) {
+    if (/^ramblings-\d+$/.test(cacheName)) {
+      let [, cacheVersion] = cacheName.match(/^ramblings-(\d+)$/);
+      cacheVersion = cacheVersion != null ? Number(cacheVersion) : cacheVersion;
+      return cacheVersion > 0 && cacheVersion != version;
+    }
+  });
+
+  return Promise.all(
+    oldCacheNames.map(function deleteCache(cacheName) {
+      return caches.delete(cacheName);
+    })
+  );
 }
